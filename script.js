4b3d6b46c5521e6da947f8b68631fd7d403371fc
Seetansh <87255041+Seetansh@users.noreply.github.com>
2025-06-05 08:38:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+const search = document.getElementById('search');
+const input = search.querySelector('.input');
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', () => {
+  search.classList.toggle('active');
+  input.focus();
+});
+
